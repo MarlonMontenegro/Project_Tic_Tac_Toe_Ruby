c6fe0d816f563fd2c9cbb4b4e915d0af992f52cd
Marlon Montenegro <marlonmontenegropaz@gmail.com>
2023-08-22 14:25:37 -0600

    Tic Tac Toe Game Finished

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class Player
+  attr_accessor :symbol
+  attr_reader :name
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+
+
+
+
+end
